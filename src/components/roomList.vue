abcf7c2a98b33ab3f7a67b1ff5f5c7a822eeb704
Anna Huang <annatointro@gmail.com>
2019-05-27 19:53:42 -0600

    add style

--- src/components/roomList.vue
+++ src/components/roomList.vue
@@ -1,22 +1,25 @@
 <template>
   <div class="allRooms">
-    hello
-    <div class="cards">
-      <ul>
-        <li v-for="card in cards">
-          <div class="info">
-            <h3>
-              {{card.title}}
-              <span v-show="card.soldOut">Sold Out</span>
-            </h3>
+    <div class="controll"></div>
 
-            <div class="price">
-              <div class="tax">+ {{tax}} % tax</div>
-            </div>
+    <div class="cards">
+      <div class="xx" v-for="card in cards">
+        <!-- --- image ----- -->
+        <img :src="card.cover">
+        <!-- --- info ---- -->
+        <div class="info">
+          <h3 class="title">{{card.title}}</h3>
+          <div class="price">
+            ${{card.price}}
+            <span class="unit">/ Night</span>
+            <mark v-show="card.soldOut">SOLD OUT</mark>
           </div>
-          <img :src="card.cover">
-        </li>
-      </ul>
+          <div class="tax">
+            Plus
+            <strong>{{tax}}</strong> % tax
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -26,41 +29,41 @@ export default {
   data() {
     return {
       basePrice: 100,
-      tax: 0,
+      tax: 10,
       cards: [
         {
-          title: "Resort",
+          title: "Beautiful Mountain Condo Pool, Hot Tub, WaterSlide",
           cover:
             "https://images.unsplash.com/photo-1520250497591-112f2f40a3f4?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp",
-          price: 0,
+          price: 145,
           soldOut: true
         },
         {
-          title: "Resort",
+          title: "Premium Penthouse - Spectacular Three View",
           cover:
             "https://images.unsplash.com/photo-1498503403619-e39e4ff390fe?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp",
-          price: 0,
-          soldOut: true
+          price: 182,
+          soldOut: false
         },
         {
-          title: "Resort",
+          title: "Luxury 2BR/2BA Chalet w Outdoor Pool & Tub",
           cover:
             "https://images.unsplash.com/photo-1542314831-068cd1dbfeeb?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp",
-          price: 0,
+          price: 225,
           soldOut: true
         },
         {
-          title: "Resort",
+          title: "Stunning Vacation Place Falcon Crest Lodge",
           cover:
             "https://images.unsplash.com/photo-1510608493064-58934ef38392?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp",
-          price: 0,
-          soldOut: true
+          price: 210,
+          soldOut: false
         },
         {
-          title: "Resort",
+          title: "Charming Spacious 2 BR for family on Legacy trail",
           cover:
             "https://images.unsplash.com/photo-1502208327471-d5dde4d78995?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp",
-          price: 0,
+          price: 195,
           soldOut: true
         }
       ]
@@ -71,12 +74,58 @@ export default {
 
 <style scoped lang="scss">
 .allRooms {
+  width: 100%;
+  .control {
+  }
   .cards {
-    width: 300px;
-    height: 500px;
-    img {
-      width: 100%;
-      background-size: cover;
+    width: 100%;
+    margin: 0;
+    padding: 0;
+    .xx {
+      display: inline-block;
+      list-style: none;
+      width: 250px;
+      height: 340px;
+      background-color: rgb(255, 255, 255);
+      margin: 0 4px;
+      padding: 10px;
+      border-radius: 3px;
+      box-shadow: 0 5px 10px rgba(black, 0.1);
+      img {
+        width: 100%;
+        background-size: cover;
+      }
+      .info {
+        .title {
+          span {
+            color: red;
+            float: right;
+          }
+        }
+        .price {
+          font-size: 32px;
+          font-weight: 500;
+          clear:both;
+
+          .unit {
+            font-size: 16px;
+          }
+          mark {
+            background-color: red;
+            color: white;
+            padding: 4px 10px;
+            font-weight: 500;
+            font-size: 13px;
+            border-radius: 3px;
+            float:right;
+          }
+        }
+        .tax {
+          font-size: 14px;
+          color: #666;
+          margin-bottom: 20px;
+        }
+      }
     }
   }
 }
