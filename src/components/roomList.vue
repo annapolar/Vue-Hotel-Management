1a8c3719e2cdeaff487467ac6d23632a97097b55
Anna Huang <annatointro@gmail.com>
2019-05-27 18:34:12 -0600

    add data

--- src/components/roomList.vue
+++ src/components/roomList.vue
@@ -1,12 +1,85 @@
 <template>
   <div class="allRooms">
-    <h2>this is room list page</h2>
+    hello
+    <div class="cards">
+      <ul>
+        <li v-for="card in cards">
+          <div class="info">
+            <h3>
+              {{card.title}}
+              <span v-show="card.soldOut">Sold Out</span>
+            </h3>
+
+            <div class="price">
+              <div class="tax">+ {{tax}} % tax</div>
+            </div>
+          </div>
+          <img :src="card.cover">
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      basePrice: 100,
+      tax: 0,
+      cards: [
+        {
+          title: "Resort",
+          cover:
+            "https://images.unsplash.com/photo-1520250497591-112f2f40a3f4?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp",
+          price: 0,
+          soldOut: true
+        },
+        {
+          title: "Resort",
+          cover:
+            "https://images.unsplash.com/photo-1498503403619-e39e4ff390fe?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp",
+          price: 0,
+          soldOut: true
+        },
+        {
+          title: "Resort",
+          cover:
+            "https://images.unsplash.com/photo-1542314831-068cd1dbfeeb?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp",
+          price: 0,
+          soldOut: true
+        },
+        {
+          title: "Resort",
+          cover:
+            "https://images.unsplash.com/photo-1510608493064-58934ef38392?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp",
+          price: 0,
+          soldOut: true
+        },
+        {
+          title: "Resort",
+          cover:
+            "https://images.unsplash.com/photo-1502208327471-d5dde4d78995?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp",
+          price: 0,
+          soldOut: true
+        }
+      ]
+    };
+  }
+};
 </script>
 
 <style scoped lang="scss">
+.allRooms {
+  .cards {
+    width: 300px;
+    height: 500px;
+    img {
+      width: 100%;
+      background-size: cover;
+    }
+  }
+}
 </style>
+
+
